0ac0b3b3f76b1a57cb5986bdcfd784d06580595e
ELIZABETH BEALE <EGBEALE@GMAIL.COM>
2022-06-20 18:07:36 -0700

    started form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends React.Component {
+  construtor(props) {
+    super(props);
+    this.state = {
+      city: '',
+      long: '',
+      lat: ''
+    }
+  }
+
+//Input handler. Gives us the data from the input.
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let cityData = await axios.get('https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json');
+      
+    }
+  }
+
+  render() {
+    return (
+      <Form>
+        <Form.Group>
+          <Form.Label></Form.Label>
+          <Form.Control
+            type='text'
+            id='cityName'
+            onChange={this.props.handleChange}
+          />
+          <button type='submit' onClick={this.props.handleSubmit}>Explore!</button>
+
+        </Form.Group>
+      </Form>
+    )
+  }
+
 
 export default App;
