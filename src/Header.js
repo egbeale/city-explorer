7b41207c6d17be224001785e5209916fae8adfc6
ELIZABETH BEALE <EGBEALE@GMAIL.COM>
2022-06-22 22:24:10 -0700

    added component files

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header>
+                <h1>CITY EXPLORER</h1>
+            </header>
+        );
+    }
+}
+
+export default Header;
